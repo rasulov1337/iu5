8c538b9c641b7270c48d7be25b2d4f10dafe9929
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-11-14 00:02:32 +0300

    Fuck this crap

---
+++ tu/lab1/ode_sin.m
@@ -0,0 +1,12 @@
+function [dx]=ode_sin(t,x)
+a0=5
+a1=4
+a2=3
+a3=1
+b=2.5
+y=sin(t)                  %/ либо 1, либо sin(t) / 
+dx=zeros(3,1)      %/ обнуление массива dx[3,1] /
+dx(1)=x(2)
+dx(2)=x(3)
+dx(3)=(b*y-a0*x(1)-a1*x(2)-a2*x(3))/a3
+end
